17acb823e534a70af691e7b8ccfcd5a453cb135e
stephenvsp <stephenvsp@gmail.com>
2021-12-01 13:18:30 -0500

    got junit in there

---
+++ src/main/kotlin/Day1.kt
@@ -0,0 +1,5 @@
+class Day1 {
+    fun one(): Int {
+        return 1
+    }
+}
---
+++ src/test/kotlin/Day1Test.kt
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+internal class Day1Test {
+
+    @Test
+    fun `day 1`() {
+        val day1 = Day1()
+        assertEquals(1, day1.one())
+    }
+}
