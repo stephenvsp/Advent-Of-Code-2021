c2b55b17ca56c194b8aa345cbf53acfbd3c4d110
stephenvsp <stephenvsp@gmail.com>
2021-12-02 09:39:05 -0500

    day 2 part 1

---
+++ src/main/kotlin/Day2.kt
@@ -0,0 +1,51 @@
+import java.io.File
+
+class Day2 {
+
+    private enum class Direction(val direction: String) {
+        FORWARD("forward"),
+        UP("up"),
+        DOWN("down"),
+    }
+
+    private data class Instruction(val direction: Direction, val distance: Int)
+
+    private fun readFile(): List<Instruction> {
+        val instructionList = mutableListOf<Instruction>()
+        File("src/main/resources/input2.txt").useLines{
+                lines -> lines.forEach {
+                    val parts = it.split(" ")
+                    val direction = when(parts[0]) {
+                        "forward" -> Direction.FORWARD
+                        "up" -> Direction.UP
+                        else -> Direction.DOWN
+                    }
+                    val newInstruction = Instruction(direction, parts[1].toInt())
+                    instructionList.add(newInstruction)
+            }
+        }
+
+        return instructionList
+    }
+
+    fun partOne(): Int {
+        val instructions = readFile()
+
+        var depth = 0
+        var horizontal = 0
+
+        instructions.forEach {
+            when(it.direction) {
+                Direction.FORWARD -> horizontal += it.distance
+                Direction.UP -> depth -= it.distance
+                Direction.DOWN -> depth += it.distance
+            }
+        }
+
+        val ans = depth * horizontal
+
+        println("Day 2 Part 1: $ans")
+
+        return ans
+    }
+}
---
+++ src/test/kotlin/Day2Test.kt
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+internal class Day2Test {
+
+    val day2 = Day2()
+
+    @Test
+    fun `day 2 part 1`() {
+        assertEquals(1507611, day2.partOne())
+    }
+}
