285bc704acfd646102407fe487e77dbd53fe106a
stephenvsp <stephenvsp@gmail.com>
2021-12-13 13:38:51 -0500

    refactoring

--- src/main/kotlin/Day12.kt
+++ src/main/kotlin/Day12.kt
@@ -2,90 +2,34 @@ import java.io.File
 
 class Day12 : Day {
 
-    private fun readFile(): Map<String, Set<String>> {
-
-        val nodes = mutableMapOf<String, MutableSet<String>>()
-
-        val lines = File("src/main/resources/input12.txt").readLines()
-
-        lines.forEach {
-            val split = it.split("-")
-
-            if (split[0] !in nodes.keys) {
-                nodes[split[0]] = mutableSetOf()
-            }
-
-            if (split[1] !in nodes.keys) {
-                nodes[split[1]] = mutableSetOf()
-            }
-
-            nodes[split[0]]!!.addAll(mutableSetOf(split[1]))
-            nodes[split[1]]!!.addAll(mutableSetOf(split[0]))
-
-
-        }
-        return nodes.toMap()
-    }
-
-    private fun isSmallCave(cave: String): Boolean {
-        return cave[0].isLowerCase()
-    }
-
-    private fun depthFirstTraversal(currentCave: String, path: MutableList<String>, smallCavesTwice: Boolean = false) {
-        if (currentCave == "end") {
-            if (!paths.contains(path)) {
-                paths.add(path)
-            }
-        }
-        else {
-            caves[currentCave]!!.forEach { nextCave ->
-                if (smallCavesTwice) {
-
-                    val smallCaveVisits = path.filter { isSmallCave(it) }.groupBy { it }.mapValues { it.value.size }
-                    val canIGoIn = if (smallCaveVisits.values.contains(2)) {
-                        !path.contains(nextCave)
-                    } else {
-                        !(nextCave == "start" || nextCave == "false")
-                    }
-
-                    if (!isSmallCave(nextCave) || canIGoIn) {
-                        val newList = mutableListOf<String>()
-                        newList.addAll(path)
-                        newList.add(nextCave)
-                        depthFirstTraversal(nextCave, newList, true)
-                    }
-                }
-                else {
-                    if (!isSmallCave(nextCave) || !path.contains(nextCave)) {
-                        val newList = mutableListOf<String>()
-                        newList.addAll(path)
-                        newList.add(nextCave)
-                        depthFirstTraversal(nextCave, newList)
-                    }
-                }
-            }
-        }
-    }
-
-
-    private val caves = readFile()
-    private var paths = mutableListOf<List<String>>()
+    private val connections = File("src/main/resources/input12.txt").readLines()
+        .map { it.split("-") }
+        .flatMap { (begin, end) -> listOf(begin to end, end to begin) }
+        .filterNot { (_, end) -> end == "start" }
+        .groupBy({ it.first }, { it.second })
 
     override fun partOne(): Int {
-        depthFirstTraversal("start", mutableListOf("start"))
-
-        val ans = paths.size
-
-        println("Day 12 Part 1: $ans")
-        return ans
+        return countPaths { cave, currentPath ->
+            cave in currentPath
+        }
     }
 
     override fun partTwo(): Int {
-        depthFirstTraversal("start", mutableListOf("start"), true)
+        return countPaths { cave, currentPath ->
+            val counts = currentPath.filter { it.first().isLowerCase() }.groupingBy { it }.eachCount()
+            cave in counts.keys && counts.any { it.value > 1 }
+        }
+    }
 
-        val ans = paths.size
-
-        println("Day 12 Part 2: $ans")
-        return ans
+    private fun countPaths(
+        cave: String = "start",
+        path: List<String> = listOf(),
+        isSmallCaveNotAllowed: (cave: String, currentPath: List<String>) -> Boolean
+    ): Int {
+        return when {
+            cave == "end" -> 1
+            cave.first().isLowerCase() && isSmallCaveNotAllowed(cave, path) -> 0
+            else -> connections.getValue(cave).sumOf { countPaths(it, path + cave, isSmallCaveNotAllowed) }
+        }
     }
 }
