72a845bdcc30b6a26d89d9ad1639ad3f983bf549
stephenvsp <stephenvsp@gmail.com>
2021-12-17 10:39:32 -0500

    day 17 part 2

--- src/main/kotlin/Day17.kt
+++ src/main/kotlin/Day17.kt
@@ -1,20 +1,29 @@
+import java.io.File
+
 class Day17 : Day {
 
-    val xRange = IntRange(288, 330)
-    val yRange = IntRange(-96, -50)
+    val input = File("src/main/resources/input17.txt").readLines().first()
+
+    val targetArea = Regex("""target area: x=(\d+)\.\.(\d+), y=(-\d+)\.\.(-\d+)""")
+        .matchEntire(input)!!
+        .destructured.let { (x1, x2, y1, y2) -> TargetArea(x1.toInt(), x2.toInt(), y1.toInt(), y2.toInt()) }
+
+    data class TargetArea(val x1: Int, val x2: Int, val y1: Int, val y2: Int) {
+        operator fun contains(point: Pair<Int, Int>) = point.first in x1..x2 && point.second in y1..y2
+    }
 
     override fun partOne(): Int {
         val madeBasketHighestYValues = mutableListOf<Int>()
 
         for (x in 1..1000) {
-            for (y in -1000..1000) {
+            for (y in 0..1000) {
                 var xVelocity = x
                 var yVelocity = y
                 var xPos = 0
                 var yPos = 0
                 val yPositions = mutableListOf<Int>()
 
-                while (!inBasket(xPos, yPos) && !missedBasket(xPos, yPos)) {
+                while (!targetArea.contains(xPos to yPos) && !missedBasket(xPos, yPos)) {
                     yPositions.add(yPos)
 
                     xPos += xVelocity
@@ -24,7 +33,7 @@ class Day17 : Day {
                     yVelocity--
                 }
 
-                if (inBasket(xPos, yPos)) {
+                if (targetArea.contains(xPos to yPos)) {
                     madeBasketHighestYValues.add(yPositions.maxOf { it })
                 }
             }
@@ -38,18 +47,44 @@ class Day17 : Day {
     }
 
     override fun partTwo(): Int {
-        TODO("Not yet implemented")
+        val madeBasketVelocity = mutableListOf<Pair<Int, Int>>()
+
+        for (x in 1..500) {
+            for (y in -100..500) {
+
+                var xVelocity = x
+                var yVelocity = y
+                var xPos = 0
+                var yPos = 0
+
+                while (!targetArea.contains(xPos to yPos) && !missedBasket(xPos, yPos)) {
+
+                    xPos += xVelocity
+                    yPos += yVelocity
+
+                    xVelocity = applyDrag(xVelocity)
+                    yVelocity--
+                }
+
+                if (targetArea.contains(xPos to yPos)) {
+                    madeBasketVelocity.add(x to y)
+                }
+            }
+        }
+
+        val ans = madeBasketVelocity.size
+
+        println("Day 17 Part 1: $ans")
+
+        return ans
     }
 
-    private fun inBasket(x: Int, y: Int): Boolean {
-        return x in xRange && y in yRange
-    }
+    private fun missedBasket(x: Int, y: Int): Boolean = missedX(x) || missedY(y)
 
-    private fun missedBasket(x: Int, y: Int): Boolean {
-        return x > xRange.last || y < yRange.last
-    }
+    private fun missedX(x: Int): Boolean = x > targetArea.x2
 
-    private fun applyDrag(x: Int): Int {
-        return if (x > 1) x - 1 else 0
-    }
+    private fun missedY(y: Int): Boolean = y < targetArea.y1
+
+    private fun applyDrag(x: Int): Int = if (x > 1) x - 1 else 0
+
 }
--- src/test/kotlin/Day17Test.kt
+++ src/test/kotlin/Day17Test.kt
@@ -9,4 +9,9 @@ internal class Day17Test {
     fun `day 17 part 1`() {
         assertEquals(4560, day17.partOne())
     }
+
+    @Test
+    fun `day 17 part 2`() {
+        assertEquals(3344, day17.partTwo())
+    }
 }
