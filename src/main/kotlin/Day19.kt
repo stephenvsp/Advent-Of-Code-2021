9e21376faee59a6d2ca59377a9c07447abe8348f
stephenvsp <stephenvsp@gmail.com>
2022-02-25 21:24:44 -0500

    wtf

--- src/main/kotlin/Day19.kt
+++ src/main/kotlin/Day19.kt
@@ -12,7 +12,7 @@ class Day19() : Day {
             Scanner(
                 s.trim().lines()
                     .map { it.split(",") }
-                    .map { (x, y, z) -> Point3D(x.toInt(), y.toInt(), z.toInt()) }
+                    .map { (x, y, z) -> Point(x.toInt(), y.toInt(), z.toInt()) }
                     .toSet()
             )
         }
@@ -26,7 +26,7 @@ class Day19() : Day {
 
     private fun assembleMap(): AssembledMap {
         val foundBeacons = scanners.first().beacons.toMutableSet()
-        val foundScannersPositions = mutableSetOf(Point3D(0, 0, 0))
+        val foundScannersPositions = mutableSetOf(Point(0, 0, 0))
 
         val remaining = ArrayDeque<Scanner>().apply { addAll(scanners.drop(1)) }
         while (remaining.isNotEmpty()) {
@@ -43,14 +43,14 @@ class Day19() : Day {
         return AssembledMap(foundBeacons, foundScannersPositions)
     }
 
-    private data class Scanner(val beacons: Set<Point3D>) {
+    private data class Scanner(val beacons: Set<Point>) {
         fun allRotations() = beacons.map { it.allRotations() }.transpose().map { Scanner(it) }
 
         fun getTransformedIfOverlap(otherScanner: Scanner): TransformedScanner? {
             return otherScanner.allRotations().firstNotNullOfOrNull { otherReoriented ->
                 beacons.firstNotNullOfOrNull { first ->
                     otherReoriented.beacons.firstNotNullOfOrNull { second ->
-                        val otherPosition = first - second
+                        val otherPosition = first + -second
                         val otherTransformed = otherReoriented.beacons.map { otherPosition + it }.toSet()
                         when ((otherTransformed intersect beacons).size >= 12) {
                             true -> TransformedScanner(otherTransformed, otherPosition)
@@ -61,7 +61,7 @@ class Day19() : Day {
             }
         }
 
-        private fun List<Set<Point3D>>.transpose(): List<Set<Point3D>> {
+        private fun List<Set<Point>>.transpose(): List<Set<Point>> {
             return when (all { it.isNotEmpty() }) {
                 true -> listOf(map { it.first() }.toSet()) + map { it.drop(1).toSet() }.transpose()
                 false -> emptyList()
@@ -69,24 +69,25 @@ class Day19() : Day {
         }
     }
 
-    private data class TransformedScanner(val beacons: Set<Point3D>, val position: Point3D)
+    private data class TransformedScanner(val beacons: Set<Point>, val position: Point)
 
-    private data class AssembledMap(val beacons: Set<Point3D>, val scannersPositions: Set<Point3D>)
+    private data class AssembledMap(val beacons: Set<Point>, val scannersPositions: Set<Point>)
 
-    private data class Point3D(val x: Int, val y: Int, val z: Int) {
-        operator fun plus(other: Point3D) = Point3D(x + other.x, y + other.y, z + other.z)
+    private data class Point(val x: Int, val y: Int, val z: Int) {
+        operator fun plus(other: Point) = Point(x + other.x, y + other.y, z + other.z)
+        operator fun unaryMinus() = Point(-this.x, -this.y, -this.z)
 
-        operator fun minus(other: Point3D) = Point3D(x - other.x, y - other.y, z - other.z)
+        operator fun minus(other: Point) = Point(x - other.x, y - other.y, z - other.z)
 
-        infix fun distanceTo(other: Point3D) = abs(x - other.x) + abs(y - other.y) + abs(z - other.z)
+        infix fun distanceTo(other: Point) = abs(x - other.x) + abs(y - other.y) + abs(z - other.z)
 
-        fun allRotations(): Set<Point3D> {
+        fun allRotations(): Set<Point> {
             return setOf(
-                Point3D(x, y, z), Point3D(x, -z, y), Point3D(x, -y, -z), Point3D(x, z, -y), Point3D(-x, -y, z),
-                Point3D(-x, -z, -y), Point3D(-x, y, -z), Point3D(-x, z, y), Point3D(-z, x, -y), Point3D(y, x, -z),
-                Point3D(z, x, y), Point3D(-y, x, z), Point3D(z, -x, -y), Point3D(y, -x, z), Point3D(-z, -x, y),
-                Point3D(-y, -x, -z), Point3D(-y, -z, x), Point3D(z, -y, x), Point3D(y, z, x), Point3D(-z, y, x),
-                Point3D(z, y, -x), Point3D(-y, z, -x), Point3D(-z, -y, -x), Point3D(y, -z, -x),
+                Point(x, y, z), Point(x, -z, y), Point(x, -y, -z), Point(x, z, -y),
+                Point(-x, -y, z), Point(-x, -z, -y), Point(-x, y, -z), Point(-x, z, y),
+                Point(-z, x, -y), Point(y, x, -z), Point(z, x, y), Point(-y, x, z), Point(z, -x, -y), Point(y, -x, z), Point(-z, -x, y),
+                Point(-y, -x, -z), Point(-y, -z, x), Point(z, -y, x), Point(y, z, x), Point(-z, y, x),
+                Point(z, y, -x), Point(-y, z, -x), Point(-z, -y, -x), Point(y, -z, -x),
             )
         }
     }
